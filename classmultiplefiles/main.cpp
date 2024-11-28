7250fc7416cd627b1ac81c0bbdec221cd83eeda9
Hussein Alsaidi <husseinw@umich.edu>
2024-11-28 06:26:43 -0500

    Update main.cpp

--- classmultiplefiles/main.cpp
+++ classmultiplefiles/main.cpp
@@ -1,1 +1,18 @@
+#include <string>
+#include <iostream>
+#include "Store.h"
+using namespace std;
+
+int main() {
+   Store ourPlace;            // Create a Store object
+   string currName;
+
+   cin >> currName;           // Input the store name
+   ourPlace.SetName(currName); // Set the store name
+
+   ourPlace.ReadAllProducts(); // Input all products
+   ourPlace.PrintSale(2);      // Print product prices after applying a discount of 2
+
+   return 0;
+}
 
